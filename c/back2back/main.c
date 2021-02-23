80cc85a200aa07b930be2264aac1aa68dd2e4625
Gabriel Marcano <gabemarcano@yahoo.com>
2021-02-23 01:28:18 -0800

    Fix strict-aliasing problem
     - Can't alias char arrays to other types, so instead explicitly model
       what we're trying to do with a struct node.
     - Weird blips happen from time to time still... I suspect it's due to
       the scheduler taking over temporarily.

--- c/back2back/main.c
+++ c/back2back/main.c
@@ -3,39 +3,44 @@
 
 #define DEBUG 0
 
+struct node
+{
+	struct node *next;
+};
+
 int main(void){
   //how many bytes to jump by
   int dists[] = {16, 32, 64};
-  char ar[64*500];
+  struct node ar[64*500/sizeof(struct node)];
 #if DEBUG
-  printf("Array bounds: %p-%p\n", ar, &ar[64*500-1]);
+  printf("Array bounds: %p-%p\n", ar, &ar[64*500/sizeof(struct node)-1]);
 #endif
 
   //call perfcycles syscall
   for(int i = 0; i < 3; i++){
-    int** startp = (int**)ar;
-    int jumpDist = dists[i]/sizeof(int*);
+    int jumpDist = dists[i]/sizeof(struct node);
 
     //set up pointers
+    struct node* startp = &ar[0];
     for(int j = 0; j < 499; j++){
-      *startp = (int*)(startp + jumpDist);//jumpDist*sizeof(int*) forward
+      startp->next = startp + jumpDist;
 #if DEBUG
-      printf("dist: %ld\n", (long)(*startp) - (long)(startp));
+      printf("dist: %ld\n", (long)(startp->next) - (long)(startp));
 #endif
       startp += jumpDist;
     }
-    *startp = NULL;
+    startp = NULL;
 #if DEBUG
     printf("end ptr: %p\n", startp);
     int count = 0;
 #endif
 
     //do test
-    int start, end;
-    startp = (int**)ar;
+    unsigned start, end;
+    startp = &ar[0];
     start = perf_cycles();
     while(startp){
-      startp = (int**)(*startp);
+      startp = startp->next;
 #if DEBUG
       count++;
 #endif
