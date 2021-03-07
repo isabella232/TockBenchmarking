6a6a6c7fd2dfe06a68173dd1fecd2f687b313e63
Gabriel Marcano <gabemarcano@yahoo.com>
2021-03-06 19:10:33 -0800

    Adjust time test
     - Time test is effectively the same as syscall, except without a system
       call in between the timestamps

--- c/time/main.c
+++ c/time/main.c
@@ -3,42 +3,28 @@
 #include <stdio.h>
 #include <perf.h>
 
-#define NUMBER_OF_SAMPLES 1000
-
-void time_test(void);
+void command_test(void);
 
 int main(void) {
-  time_test();
+  command_test();
   return 0;
 }
 
-void time_test(void)
+void command_test(void)
 {
-  unsigned cycles[NUMBER_OF_SAMPLES];
-  unsigned instructions[NUMBER_OF_SAMPLES];
-
-  for(size_t i = 0; i < NUMBER_OF_SAMPLES; ++i) {
+  printf("### RESULTS ###\n");
+  printf("benchmark: timing_overhead_cycles\n");
+  printf("run, cycles\n");
+  for(size_t i = 0; i < 1000; ++i) {
     unsigned cycles1 = perf_cycles();
     unsigned cycles2 = perf_cycles();
-    cycles[i] = cycles2 - cycles1;
+    printf("%u, %u\n", i, cycles2 - cycles1);
   }
-  
-  for(size_t i = 0; i < NUMBER_OF_SAMPLES; ++i) {
+  printf("benchmark: timing_overhead_instructions\n");
+  printf("run, instructions\n");
+  for(size_t i = 0; i < 1000; ++i) {
     unsigned instr1 = perf_instructions_retired();
     unsigned instr2 = perf_instructions_retired();
-    instructions[i] = instr2 - instr1;
-  }
-  printf("### RESULTS ###\n");
-  printf("benchmark: time_overhead_cycles\n");
-  printf("run, cycles\n");
-  for(size_t i = 0; i < NUMBER_OF_SAMPLES; i++){
-    printf("%u, %u\n", i, cycles[i]);
-  }
-
-  printf("\n### RESULTS ###\n");
-  printf("benchmark: time_overhead_instructions\n");
-  printf("run, instructions\n");
-  for(size_t i = 0; i < NUMBER_OF_SAMPLES; i++){
-    printf("%u, %u\n", i, instructions[i]);
+    printf("%u, %u\n", i, instr2 - instr1);
   }
 }
