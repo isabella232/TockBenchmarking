c9383f4fd7fe4c9e08fd7270b19d536e5817e2af
Max <maximilian.paulsen@gmail.com>
2021-03-07 14:32:44 -0800

    renamed threading benchmarks and added a client -> server time

--- c/ipc_threading_client/main.c
+++ c/ipc_threading_client/main.c
@@ -16,6 +16,39 @@ uint32_t samples[SAMPLE_COUNT];
 
 size_t counter;
 
+uint32_t static_cycles;
+
+static void ipc_callback_inverse(__attribute__((unused)) int pid,
+                         __attribute__((unused)) int len,
+                         __attribute__((unused)) int buf_ptr, __attribute__((unused)) void *ud)
+{
+  uint32_t other_cycles = *buf;
+  samples[counter] = other_cycles - static_cycles;
+  counter++;
+  if (counter < SAMPLE_COUNT)
+  {
+    static_cycles = perf_cycles();
+    ipc_notify_svc(ipc_svc_num);
+  }
+  else
+  {
+    uint32_t min = samples[1];
+    for (size_t i = 2; i < SAMPLE_COUNT; i++)
+    {
+      min = samples[i] < min ? samples[i] : min;
+    }
+    printf("Min time is: %ld cycles\n", min);
+    printf("### RESULTS ###\n");
+    printf("benchmark:, measures ipc call overhead from client to server\n");
+    printf("description:, Measures cycles taken for one process to notify the other\n");
+    printf("run, cycles\n");
+    for (size_t i = 1; i < SAMPLE_COUNT; i++)
+    {
+      printf("%d, %ld\n", i, samples[i]);
+    }
+  }
+}
+
 static void ipc_callback(__attribute__((unused)) int pid,
                          __attribute__((unused)) int len,
                          __attribute__((unused)) int buf_ptr, __attribute__((unused)) void *ud)
@@ -37,15 +70,19 @@ static void ipc_callback(__attribute__((unused)) int pid,
     }
     printf("Min time is: %ld cycles\n", min);
     printf("### RESULTS ###\n");
-    printf("benchmark:, measures ipc call overhead\n");
+    printf("benchmark:, measures ipc call overhead from server to client\n");
     printf("description:, Measures cycles taken for one process to notify the other\n");
     printf("run, cycles\n");
     for (size_t i = 0; i < SAMPLE_COUNT; i++)
     {
       printf("%d, %ld\n", i, samples[i]);
     }
+    counter = 0;
+    ipc_register_client_cb(ipc_svc_num, ipc_callback_inverse, buf);
+    ipc_notify_svc(ipc_svc_num);
   }
 }
+
 
 int main(void)
 {
