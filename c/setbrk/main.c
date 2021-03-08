fb61e9747fc8cb03d3e53a5ced9d702e612c31b7
Gabriel Marcano <gabemarcano@yahoo.com>
2021-03-07 19:00:01 -0800

    setbrk: preliminary data
     - printf refactoring

--- c/setbrk/main.c
+++ c/setbrk/main.c
@@ -16,22 +16,17 @@ int main(void) {
 
 void setbrk_test(void)
 {
-  unsigned times[NUMBER_OF_SAMPLES];
-  void* breaks[NUMBER_OF_SAMPLES];
+  printf("### RESULTS ###\n");
+  printf("benchmark: setbrk_overhead\n");
+  printf("description: changes the heap break which requires an MPU update\n");
+  printf("run, cycles, break, break_decimal\n");
 
   for(size_t i = 0; i < NUMBER_OF_SAMPLES; ++i) {
     unsigned cycles1 = perf_cycles();
     void* old_break = memop(1, BREAK_STRIDE);
     unsigned cycles2 = perf_cycles();
-    times[i] = cycles2 - cycles1;
-    breaks[i] = (uint8_t*)old_break + BREAK_STRIDE;
-  }
-  
-  printf("### RESULTS ###\n");
-  printf("benchmark: setbrk_overhead\n");
-  printf("description: changes the heap break which requires an MPU update\n");
-  printf("run, cycles, break, break_decimal\n");
-  for(size_t i = 0; i < NUMBER_OF_SAMPLES; i++){
-    printf("%u, %u, %p, %u\n", i, times[i], breaks[i], (size_t)breaks[i]);
+    unsigned time = cycles2 - cycles1;
+	void *break_ = (uint8_t*)old_break + BREAK_STRIDE;
+    printf("%u, %u, %p, %u\n", i, time, break_, (size_t)break_);
   }
 }
