022a8d3c5e5ba536737e38e0e47e1d95050990ab
Gabriel Marcano <gabemarcano@yahoo.com>
2021-03-07 17:29:04 -0800

    ipc_threading: preliminary data

--- c/ipc_threading_client/main.c
+++ c/ipc_threading_client/main.c
@@ -10,42 +10,22 @@
 
 int ipc_svc_num = 0;
 
-uint32_t buf[64 / sizeof(uint32_t)] __attribute__((aligned(64)));
-
-uint32_t samples[SAMPLE_COUNT];
-
-size_t counter;
+uint32_t buf[64 / sizeof(uint32_t)];
 
 uint32_t static_cycles;
+size_t counter;
 
 static void ipc_callback_inverse(__attribute__((unused)) int pid,
                          __attribute__((unused)) int len,
                          __attribute__((unused)) int buf_ptr, __attribute__((unused)) void *ud)
 {
   uint32_t other_cycles = *buf;
-  samples[counter] = other_cycles - static_cycles;
+  printf("%d, %ld\n", counter, other_cycles - static_cycles);
   counter++;
   if (counter < SAMPLE_COUNT)
   {
     static_cycles = perf_cycles();
     ipc_notify_svc(ipc_svc_num);
-  }
-  else
-  {
-    uint32_t min = samples[1];
-    for (size_t i = 2; i < SAMPLE_COUNT; i++)
-    {
-      min = samples[i] < min ? samples[i] : min;
-    }
-    printf("Min time is: %ld cycles\n", min);
-    printf("### RESULTS ###\n");
-    printf("benchmark:, measures ipc call overhead from client to server\n");
-    printf("description:, Measures cycles taken for one process to notify the other\n");
-    printf("run, cycles\n");
-    for (size_t i = 1; i < SAMPLE_COUNT; i++)
-    {
-      printf("%d, %ld\n", i, samples[i]);
-    }
   }
 }
 
@@ -55,7 +35,7 @@ static void ipc_callback(__attribute__((unused)) int pid,
 {
   uint32_t cycles = perf_cycles();
   uint32_t other_cycles = *buf;
-  samples[counter] = cycles - other_cycles;
+  printf("%d, %ld\n", counter, cycles - other_cycles);
   counter++;
   if (counter < SAMPLE_COUNT)
   {
@@ -63,26 +43,15 @@ static void ipc_callback(__attribute__((unused)) int pid,
   }
   else
   {
-    uint32_t min = samples[0];
-    for (size_t i = 1; i < SAMPLE_COUNT; i++)
-    {
-      min = samples[i] < min ? samples[i] : min;
-    }
-    printf("Min time is: %ld cycles\n", min);
     printf("### RESULTS ###\n");
-    printf("benchmark:, measures ipc call overhead from server to client\n");
+    printf("benchmark:, measures ipc call overhead from client to server\n");
     printf("description:, Measures cycles taken for one process to notify the other\n");
     printf("run, cycles\n");
-    for (size_t i = 0; i < SAMPLE_COUNT; i++)
-    {
-      printf("%d, %ld\n", i, samples[i]);
-    }
     counter = 0;
     ipc_register_client_cb(ipc_svc_num, ipc_callback_inverse, buf);
     ipc_notify_svc(ipc_svc_num);
   }
 }
-
 
 int main(void)
 {
@@ -99,8 +68,12 @@ int main(void)
   printf("Sharing buffer at %x\n", (size_t)buf);
 
   ipc_register_client_cb(ipc_svc_num, ipc_callback, buf);
+  ipc_share(ipc_svc_num, buf, sizeof(buf));
 
-  ipc_share(ipc_svc_num, buf, 64);
+  printf("### RESULTS ###\n");
+  printf("benchmark:, measures ipc call overhead from server to client\n");
+  printf("description:, Measures cycles taken for one process to notify the other\n");
+  printf("run, cycles\n");
 
   ipc_notify_svc(ipc_svc_num);
   return 0;
