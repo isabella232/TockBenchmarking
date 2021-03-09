5f88c7727ffb8c0820b15f2827e1127d72960297
Gabriel Marcano <gabemarcano@yahoo.com>
2021-03-09 01:19:52 -0800

    memory_bandwidth: overhaul
     - make tests more similar to lmbench ones, now testing more buffer
       sizes

--- c/bandwidth/main.c
+++ c/bandwidth/main.c
@@ -3,11 +3,11 @@
 
 #define BUF_SIZE (1u << 14)
 
-const char flash[BUF_SIZE] = "this is a test buffer";
-
-#define TEST(NAME, LINE, PRE, POST) \
-	printf("benchmark: " #NAME ", size %u\n", BUF_SIZE);\
+#define TEST(NAME, LINE, PRE, POST, SIZE) \
+{\
+	printf("benchmark: " #NAME ", size %u\n", SIZE);\
 	printf("run, cycles\n");\
+	const size_t SIZE_ = SIZE;\
 	for (size_t i  = 0; i < 100; i++)\
 	{\
 		PRE;\
@@ -17,7 +17,7 @@ const char flash[BUF_SIZE] = "this is a test buffer";
 		POST;\
 		printf("%u, %u\n", i, end-begin);\
 	}\
-	printf("benchmark: " #NAME ", size %u\n", BUF_SIZE);\
+	printf("benchmark: " #NAME ", size %u\n", SIZE);\
 	printf("run, instructions\n");\
 	for (size_t i  = 0; i < 100; i++)\
 	{\
@@ -27,36 +27,48 @@ const char flash[BUF_SIZE] = "this is a test buffer";
 		unsigned end = perf_instructions_retired();\
 		POST;\
 		printf("%u, %u\n", i, end-begin);\
-	}
+	}\
+}
 
+#define TEST_SET(NAME, LINE, PRE, POST) \
+	TEST(NAME, LINE, PRE, POST, 1 << 5) \
+	TEST(NAME, LINE, PRE, POST, 1 << 6) \
+	TEST(NAME, LINE, PRE, POST, 1 << 7) \
+	TEST(NAME, LINE, PRE, POST, 1 << 8) \
+	TEST(NAME, LINE, PRE, POST, 1 << 9) \
+	TEST(NAME, LINE, PRE, POST, 1 << 10) \
+	TEST(NAME, LINE, PRE, POST, 1 << 11) \
+	TEST(NAME, LINE, PRE, POST, 1 << 12) \
+	TEST(NAME, LINE, PRE, POST, 1 << 13) \
+	TEST(NAME, LINE, PRE, POST, 1 << 14)
+
+unsigned sram1[BUF_SIZE/4];
+unsigned sram2[BUF_SIZE/4];
 
 int main(void)
 {
-	volatile unsigned sram1[BUF_SIZE/4];
-	volatile unsigned sram2[BUF_SIZE/4];
-
 	// memcpy
-	printf("sram locations: %p %p\n", sram1, sram2);
-	printf("flash locations: %p\n", flash);
 	printf("### RESULTS ###\n");
-	TEST(memcpy_bandwidth_cycles, memcpy((char*)sram1, (char*)sram2, sizeof(sram1)),,)
-	TEST(manual_copy_bandwidth_cycles,
-		for (size_t j = 0; j < sizeof(sram1)/4; ++j) sram1[j] = sram2[j];,
+	for (size_t j = 0; j < sizeof(sram1)/4; ++j) sram2[j] = perf_cycles();
+	TEST_SET(memcpy_bandwidth_cycles, memcpy(sram1, sram2, SIZE_),,)
+	TEST_SET(manual_copy_bandwidth_cycles,
+		for (size_t j = 0; j < SIZE_/4; ++j) sram1[j] = sram2[j];,
 		,
 	)
 
+	for (size_t j = 0; j < sizeof(sram1)/4; ++j) sram1[j] = perf_cycles();
 	unsigned sum = 0;
-	TEST(read_bandwidth_instructions,
-		for (size_t j = 0; j < sizeof(sram1)/4; ++j) val = sram1[j];,
-		volatile unsigned val;,
-		for (size_t j = 0; j < sizeof(sram1)/4; ++j) sum += sram1[j] + val;
+	TEST_SET(read_bandwidth_instructions,
+		for (size_t j = 0; j < SIZE_/4; ++j) sum += sram1[j];
+		,
+		,
 	)
-	printf("Ignore this value: %u\n", sum);
 
-	TEST(write_bandwidth_instructions,
-		for (size_t j = 0; j < sizeof(sram1)/4; ++j) sram1[j] = val;,
-		volatile unsigned val = 0xFFFF0000 + i,
-		for (size_t j = 0; j < sizeof(sram1)/4; ++j) sum += sram1[j];
+	for (size_t j = 0; j < sizeof(sram1)/4; ++j) sram1[j] = perf_cycles();
+	TEST_SET(write_bandwidth_instructions,
+		for (size_t j = 0; j < SIZE_/4; ++j) sram1[j] = val;,
+		unsigned val = 0xFFFF0000 + i,
+		for (size_t j = 0; j < SIZE_/4; ++j) sum += sram1[j];
 	)
 	printf("Ignore this value: %u\n", sum);
 
